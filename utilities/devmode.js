bbf109a601ba6c649413d2df7072cf47cf1a32de
NovaCSClubManassas <csclubnvccmanassas@gmail.com>
2025-12-08 17:58:56 -0500

    Add files via upload

---
+++ utilities/devmode.js
@@ -0,0 +1,7 @@
+// ##################################
+// WARNING:
+// If devMode is set to true, 
+// it will have populate.js call puppet-json.js 
+// instead of the firebase api
+// ##################################
+export const devMode = false;
